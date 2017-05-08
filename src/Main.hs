977d1baa1cbcbfffe89b5c0f31d38b99734e7a71
Matthew Daley <mattd@bugfuzz.com>
2017-05-08 23:20:33 +1200

    Trying to fix distance algo, cube generation, etc.

--- src/Main.hs
+++ src/Main.hs
@@ -13,8 +13,8 @@ import Codec.Picture
 import Data.Vect.Float
 import Data.Vect.Float.Base
 
-data Ray = Ray Vec3 Vec3
-data LineSeg = LineSeg Vec3 Vec3
+data Ray = Ray Vec3 Vec3 deriving Show
+data LineSeg = LineSeg Vec3 Vec3 deriving Show
 
 lineRayDistance :: Ray -> LineSeg -> Float
 lineRayDistance (Ray a0 a1) (LineSeg b0 b1) = if denom < epsilon then parallel else nonparallel
@@ -32,49 +32,70 @@ lineRayDistance (Ray a0 a1) (LineSeg b0 b1) = if denom < epsilon then parallel e
         d1 = _A &. (b1 &- a0)
         parallel = if d0 <= magA && d1 <= magA
                        then norm (a1 &- (if abs d0 < abs d1 then b0 else b1))
-                       else norm ((_A &* d0) &+ a0 &- b0)
+                       else
+                           if d0 >= magA && d1 >= magA
+                           then norm (a1 &- (if abs d0 < abs d1 then b0 else b1))
+                           else norm ((_A &* d0) &+ a0 &- b0)
         t = b0 &- a0
         detA = det3 t _B cross
         detB = det3 t _A cross
         t0 = detA / denom
         t1 = detB / denom
-        pA = if t0 > magA then a1 else a0 &+ (_A &* t0)
-        dt = _B &. (pA &- b0)
-        pB = if t0 > magA then b0 &+ (_B &* dt) else b0 &+ (_B &* t1)
-        nonparallel = norm (pA &- pB)
+        pA = a0 &+ (_A &* t0)
+        pB = b0 &+ (_B &* t1)
+        pA' = if t0 > magA then a1 else pA
+        dt = _B &. (pA' &- b0)
+        pB' = if t1 < 0 then b0 else (if t1 > magB then b1 else pB)
+        clamp a b x = if x < a then a else (if x > b then b else x)
+        dotA = _B &. (pA &- b0)
+        pB'' = if t0 > magA then b0 &+ (_B &* (clamp 0 magB dotA)) else pB'
+        dotB = _A &. (pB &- a0)
+        pA'' = if t1 < 0 || t1 > magB then a0 &+ (_A &* (clamp 0 magA dotB)) else pA'
+        nonparallel = norm (pA'' &- pB'')
 
 rotateCCW90 :: Pixel a => Image a -> Image a
 rotateCCW90 image@Image {imageWidth, imageHeight} = generateImage pixel imageHeight imageWidth
     where
         pixel x y = pixelAt image ((imageWidth - 1) - y) x
 
+cube :: [LineSeg]
+cube = [LineSeg (cubeVerts !! a) (cubeVerts !! b) | b <- [0..7], a <- [0..b], oneCompDiff (cubeVerts !! a) (cubeVerts !! b)]
+    where
+        comp n i = if n `mod` (i * 2) >= i then 0.5 else -0.5
+        cubeVerts = [Vec3 (comp n 1) (comp n 2) (comp n 4) | n <- [0..7]]
+        oneCompDiff (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) = length (filter id $ zipWith (==) [x1, y1, z1] [x2, y2, z2]) == 2
+
+rotate :: Float -> Float -> Float -> Vec3 -> Vec3
+rotate rx ry rz v = (rotMatrixX rz) *. (rotMatrixY ry) *. (rotMatrixZ rx) *. v
+
+rotateLineSeg :: Float -> Float -> Float -> LineSeg -> LineSeg
+rotateLineSeg rx ry rz (LineSeg v1 v2) = LineSeg (rotate rx ry rz v1) (rotate rx ry rz v2)
+
 cubeImage :: Int -> Int -> Float -> Image PixelRGB8
 cubeImage width height t = generateImage pixel width height
     where
         pixel x y = PixelRGB8 d d d
             where
-                md = fromIntegral $ min width height
-                r = Ray (Vec3 0 0 (-100))
+                md = (fromIntegral $ min width height) / 3
+                r = Ray (Vec3 0 0 (-2))
                         (Vec3 (((fromIntegral x) - ((fromIntegral width) / 2)) / md)
                               (((fromIntegral y) - ((fromIntegral height) / 2)) / md)
                               0)
-                comp n i = if n `mod` (i * 2) >= i then 1 else -1
-                cubeVerts = [Vec3 (comp n 1) (comp n 2) (comp n 4) | n <- [0..7]]
-                rx = cos ( t        * 2 * pi)
-                ry = cos ((t + 0.3) * 2 * pi)
-                rz = cos ((t + 0.7) * 2 * pi)
-                rotCubeVerts = [(rotMatrixX rx) *. (rotMatrixY ry) *. (rotMatrixZ rz) *. v | v <- cubeVerts]
-                cube = [LineSeg (cubeVerts !! n) (rotCubeVerts !! ((n + 2) `mod` (length rotCubeVerts))) | n <- [0..7]]
-                d = floor $ min ((minimum (map (lineRayDistance r) cube)) * 2000) 255
+                rx =  t        * 0 * pi
+                ry = (t + 0.3) * 2 * pi
+                rz = (t + 0.7) * 0 * pi
+                rotCube = map (rotateLineSeg rx ry rz) cube
+                d = floor $ min ((minimum (map (lineRayDistance r) rotCube)) * 4000) 255
 
 width  = 600
-height = 200
+height = 480
 
 main = do
     args <- getArgs
     let filenameFormat = args !! 0
 
-    let numFrames = 100 :: Int
+    let numFrames = 20 :: Int
     forM_ [0..(numFrames - 1)] $ \frameNo -> do
+        putStrLn (printf "%d / %d..." (frameNo + 1) numFrames)
         let cube = cubeImage width height ((fromIntegral frameNo) / (fromIntegral numFrames))
         writeBitmap (printf filenameFormat frameNo) $ {-rotateCCW90-} cube
