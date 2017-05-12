823663af624fed50a370f28b92bf73515b3069e1
Matthew Daley <mattd@bugfuzz.com>
2017-05-12 20:02:17 +1200

    Bunch of fixes

--- src/Main.hs
+++ src/Main.hs
@@ -55,15 +55,15 @@ rotateCCW90 image@Image {imageWidth, imageHeight} = generateImage pixel imageHei
     where
         pixel x y = pixelAt image ((imageWidth - 1) - y) x
 
-cube :: [LineSeg]
-cube = (zipWith color [(cubeVerts !! a, cubeVerts !! b) | b <- [0..7], a <- [0..b], oneCompDiff (cubeVerts !! a) (cubeVerts !! b)] [0..]) ++ [LineSeg v (v &+ (Vec3 epsilon 0 0)) (Vec3 1000 1000 1000) | v <- cubeVerts]
+cube :: Float -> [LineSeg]
+cube rad = (zipWith color [(cubeVerts !! a, cubeVerts !! b) | b <- [0..7], a <- [0..b], oneCompDiff (cubeVerts !! a) (cubeVerts !! b)] [0..]) ++ [LineSeg v (v &+ (Vec3 epsilon 0 0)) (Vec3 1000 1000 1000) | v <- cubeVerts]
     where
         epsilon = 1e-6
         comp n i = n `mod` (i * 2) >= i
-        vcomp n i = if comp n i then 0.5 else -0.5
+        vcomp n i = if comp n i then rad else -rad
         cubeVerts = [Vec3 (vcomp n 1) (vcomp n 2) (vcomp n 4) | n <- [0..7]]
         ccomp n i = if comp n i then 255 else 127
-        color (a, b) n = let n' = n `mod` 6 + 1 in LineSeg a b (Vec3 0 (ccomp n' 1) (ccomp n' 2))
+        color (a, b) n = let n' = n `mod` 6 + 1 in LineSeg a b (Vec3 255 255 64)--0 (ccomp n' 1) (ccomp n' 2))
         oneCompDiff (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) = length (filter id $ zipWith (==) [x1, y1, z1] [x2, y2, z2]) == 2
 
 rotate :: Float -> Float -> Float -> Vec3 -> Vec3
@@ -76,31 +76,29 @@ orthoCamera :: Float -> Float -> Float -> Float -> Float -> Ray
 orthoCamera zoom width height x y = Ray (Vec3 0 0 (-2))
                                         (Vec3 ((x - (width  / 2)) / md / zoom)
                                               ((y - (height / 2)) / md / zoom)
-                                         0)
+                                              0)
     where
         md = min width height
 
 circularCamera :: Float -> Float -> Float -> Float -> Float -> Ray
-circularCamera zoom width height x y = Ray (Vec3 0 0 (-2))
-                                           (Vec3 (cos ((-x / width * 2 - 0.5) * pi) * zoom)
-                                                 ((y - (height / 2)) / md / zoom)
-                                                 (sin ((-x / width * 2 - 0.5) * pi) * zoom))
-    where
-        md = min width height
+circularCamera rad width height x y = Ray (Vec3 0 0 (-2))
+                                          (Vec3 (cos ((-x / width * 2 + 0.5) * pi) * rad)
+                                                ((y / height - 0.5) * 2 * rad)
+                                                (sin ((-x / width * 2 + 0.5) * pi) * rad))
 
 onComp :: (Float -> Float -> Float) -> Vec3 -> Vec3 -> Vec3
 onComp f (Vec3 a b c) (Vec3 x y z) = Vec3 (f a x) (f b y) (f c z)
 
-render :: Int -> Int -> (Float -> Float -> Float -> Float -> Ray) -> [LineSeg] -> Image PixelRGB8
-render width height camera lineSegs = generateImage pixel width height
+render :: Int -> Int -> (Float -> Float -> Float -> Float -> Ray) -> Int -> [LineSeg] -> Image PixelRGB8
+render width height camera fade lineSegs = generateImage pixel width height
     where
         pixel x y = PixelRGB8 (floor r) (floor g) (floor b)
             where
                 t = camera (fromIntegral width) (fromIntegral height) (fromIntegral x) (fromIntegral y)
-                Vec3 r g b = onComp min (Vec3 255 255 255) $ foldr1 (&+) [c &* (1 / ((lineRayDistance t l + 1) ^ 50)) | l@(LineSeg _ _ c) <- lineSegs]
+                Vec3 r g b = onComp min (Vec3 255 255 255) $ foldr1 (&+) [c &* (1 / ((lineRayDistance t l + 1) ^ fade)) | l@(LineSeg _ _ c) <- lineSegs]
 
-width  = 600
-height = 480
+width  = floor $ (fromIntegral height) * pi
+height = 200
 
 main = do
     args <- getArgs
@@ -110,9 +108,9 @@ main = do
     forM_ [0..(numFrames - 1)] $ \frameNo -> do
         putStrLn (printf "%d / %d..." (frameNo + 1) numFrames)
         let t = (fromIntegral frameNo) / (fromIntegral numFrames)
-        let rx = (t + 0.7) * ( 4) * pi
-        let ry =  t        * (-2) * pi
-        let rz = (t + 0.2) * ( 2) * pi
-        let rotCube = map (rotateLineSeg rx ry rz) cube
-        let image = render width height (orthoCamera 0.4) rotCube
-        writeBitmap (printf filenameFormat frameNo) $ {-rotateCCW90-} image
+        let rx = 0--(t + 0.7) * ( 4) * pi
+        let ry = t * 2 * pi-- t        * (-2) * pi
+        let rz = 0--(t + 0.2) * ( 2) * pi
+        let rotCube = map (rotateLineSeg rx ry rz) (cube 0.33)
+        let image = render width height (circularCamera (sqrt (2 * 0.5 ^ 2) + 0.1)) 50 rotCube
+        writeBitmap (printf filenameFormat frameNo) $ rotateCCW90 image
